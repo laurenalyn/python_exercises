69df3c674fa948dd04f66d67c6cbd91aadef39d1
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-15 20:51:32 -0400

    EX1: Prompts user for his name using input function

---
+++ exercise_1.py
@@ -0,0 +1,15 @@
+# Hello, you!
+# Prompt the user for his name using the input function. Example:
+
+# name = input('What is your name? ')
+# Upon receiving his name, you will say hello to him. Example session:
+
+# $ python hello.py
+# What is your name? Chris
+# Hello, Chris!
+
+name = input("What is your name? ")
+print("Hello, " + name)
+
+# Output : What is your name? Lauren
+# Hello, Lauren
