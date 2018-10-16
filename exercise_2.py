ae0320acacea7e2df91fca730c0e031a598aa5e2
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-16 11:32:37 -0400

    Same as the first exercise, but this time print the user's name in ALL CAPS, and also tell them the number of letters in their name.

---
+++ exercise_2.py
@@ -0,0 +1,9 @@
+name = input("What is your name? ")
+print("HELLO, " + name.upper())
+length = len(name)
+print("Your name has ".upper() + str(length) + " letters in it!".upper())
+# OUTPUT BELOW
+
+# What is your name? lauren
+# HELLO, LAUREN
+# YOUR NAME HAS 6 LETTERS IN IT!
