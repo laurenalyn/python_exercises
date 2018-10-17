2d12436b4b0855ebdce5c7a74c292dd3ee5d2272
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-17 17:25:24 -0400

    clean up the code

--- exercise_5.py
+++ exercise_5.py
@@ -1,29 +1,30 @@
 # Work or Sleep In?
-# Prompt the user for a day of the week just like the previous problem. 
-# But this time, print "Go to work" if it's a work day and "Sleep in" if 
-# it's a weekend day. 
+# Prompt the user for a day of the week just like the previous problem. But this time, print "Go to work" if it's a work day and "Sleep in" if it's a weekend day. Example session:
 
-day = int(input("Please enter a number from 0 - 6: "))
+day = int(raw_input("Please enter a number from 0 - 6 "))
 
-if (day == 0):
+# Fill in your code here
+# The user will enter a number between 0 to 6 inclusive. Given this number, 
+# print a day of the week. 0 for Sunday, 1 for Monday, 2 for Tuesday, 
+# and so on. Here's an example user session (this assumes you've named the 
+# file exercise_5.py):
+if day == 0:
     print("Sleep in")
-elif (day == 1):
+elif day == 1:
     print("Go to work")
-elif (day == 2):
+elif day == 2:
     print("Go to work")
-elif (day == 3):
+elif day == 3:
     print("Go to work")
-elif (day == 4):
+elif day == 4:
     print("Go to work")
-elif (day == 5):
+elif day == 5:
     print("Go to work")
-elif (day == 6):
+elif day == 6:
     print("Sleep in")
-
-
-# $ python exercise_5.py
+# $ python work_or_sleep_in.py
 # Day (0-6)? 5
 # Go to work
-# $ exercise_5.py
+# $ python work_or_sleep_in.py
 # Day (0-6)? 6
 # Sleep in
