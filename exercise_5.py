356c3133b864061aaecf10a6f4dd34e570a81a03
laurenalyn <laurenalynwilkerson@gmail.com>
2018-10-17 15:16:40 -0400

    Using if/elif statements again - this time if its a weekday we say 'Go to work' weekend 'Sleep in'

---
+++ exercise_5.py
@@ -0,0 +1,29 @@
+# Work or Sleep In?
+# Prompt the user for a day of the week just like the previous problem. 
+# But this time, print "Go to work" if it's a work day and "Sleep in" if 
+# it's a weekend day. 
+
+day = int(input("Please enter a number from 0 - 6: "))
+
+if (day == 0):
+    print("Sleep in")
+elif (day == 1):
+    print("Go to work")
+elif (day == 2):
+    print("Go to work")
+elif (day == 3):
+    print("Go to work")
+elif (day == 4):
+    print("Go to work")
+elif (day == 5):
+    print("Go to work")
+elif (day == 6):
+    print("Sleep in")
+
+
+# $ python exercise_5.py
+# Day (0-6)? 5
+# Go to work
+# $ exercise_5.py
+# Day (0-6)? 6
+# Sleep in
